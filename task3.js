3a11c1de53d7d7917deb3697c69b293ecaf92ad7
oyama <oyama@projectcodex.co>
2015-03-05 15:38:26 +0200

    task3.js is done

---
+++ task3.js
@@ -0,0 +1,9 @@
+ function myRedColor (){
+        	document.getElementById("boxed2").style.backgroundColor = "red";
+
+        }
+
+function myGreenColor (){
+			document.getElementById("boxed2").style.backgroundColor = "green";
+
+}
