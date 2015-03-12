943b41577dde8dcee5abdda19cb54e07abfe8056
oyama <oyama@projectcodex.co>
2015-03-12 10:50:31 +0200

    Task5 is done

---
+++ Task5/task5_tests.js
@@ -0,0 +1,21 @@
+// change to green
+setBackgroundColor('mySquare','green');
+
+// is it green?
+if(document.getElementById('mySquare').style.backgroundColor === 'green'){
+		window.alert("Success!");
+
+}else {
+	document.getElementById('mySquare').style.backgroundColor === 'red';
+	window.alert("Failure!");}
+
+// change to red
+setBackgroundColor('mySquare','red');
+// is it red?
+if(document.getElementById('mySquare').style.backgroundColor === 'red'){
+		window.alert("Failure!");
+
+}else {
+	document.getElementById('mySquare').style.backgroundColor === 'green';
+	window.alert("Success!");}
+
